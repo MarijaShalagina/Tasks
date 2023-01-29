be36ae346cb3e6127c74e19f4e64c3b17f15a546
Marija <manjasha2904@mail.ru>
2023-01-30 03:04:12 +0700

    Tasks5/

---
+++ Tasks5/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая принимает на вход трехзначное число и на выходе показывает последнюю цифру этого числа 456=6
+Console.Clear();
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+if (num < 100 || num > 999){
+    Console.WriteLine("Введено неправильное число");
+    return;
+}
+int otvet = num % 10;
+Console.WriteLine($"3 цифра: {otvet}");
